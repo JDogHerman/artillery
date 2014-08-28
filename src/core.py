32caabf5e458ee8eb3ac3550218d669d00e78dbb
Justin <justin.herman@altercareonline.net>
2014-08-28 15:54:53 -0400

    Renamed file and added call to it in core.py

--- src/core.py
+++ src/core.py
@@ -58,10 +58,12 @@ def ban(ip):
 				shun = read_config("SHUN").lower()
 				if shun == "on":
 					#call shun_v2_encripted to run
-					print "Need to call shun script to run"
-					filewrite = file("/var/artillery/banlist.txt", "a")
-					filewrite.write(ip+"\n")
-					filewrite.close()
+                                        print "Calling Shun Script"
+										subprocess.Popen("cd /var/artillery/src;shun.py %s 1" % ip, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                                        filewrite = file("/var/artillery/banlist.txt", "a")
+                                        filewrite.write(ip+"\n")
+                                        filewrite.close()
+
 				else:
 					filewrite = file("/var/artillery/banlist.txt", "a")
 					subprocess.Popen("iptables -I ARTILLERY 1 -s %s -j DROP" % ip, shell=True).wait()
