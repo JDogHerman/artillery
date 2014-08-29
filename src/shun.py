0654398f3daf838683e98a2144be2733891d74bf
justin <justin@ubuntu.(none)>
2014-08-29 16:33:14 -0400

    got shun.py working

--- src/shun.py
+++ src/shun.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python
 #########################################################################################################
 #
+# Automatic Shunning Script for ArcSight
+#
 # Takes in two parameters, attacker IP address and shun or unshun
 #
 # Note: python-pexpect will need to be installed on the machine
 #
-# Written by: TrustedSec - David Kennedy
+# Originally Written by: TrustedSec - David Kennedy
 #
-# Instructions: There are two modules that will need to be installed for
-# this to operate properly. The first is python-pexpect and the second is
-# python-pycrypto.           
+# Intructions: There is a  module that will need to be installed for
+# this to operate properly.           
 # 
-# Download and install pycrypto: https://www.dlitz.net/software/pycrypto/
 # Download and install pexpect: http://www.noah.org/wiki/pexpect#Download_and_Installation
-#
-# Once pycrypto is installed, run the second tool (pycrypt.py )provided, encrypt password. Place the 
-# output of that into the USER_PASSWORD and ENABLE_PASSWORD fields. 
+# 
 #
 #########################################################################################################
 #
 # Version 0.1 - Initial release for shunning in ArcSight
 # Version 0.2 - Added capability for Advanced Encryption Standard (AES)
+# Version 0.2.1 - Ripped out AES
 #
 ##########################################################################################################
 
 import subprocess
 import pexpect
 import sys
-import thread
 import os
 import base64
 
 # Version tracking
 VERSION = "0.2"
-
 
 # Specify username and password for firewalls
 USERNAME = "test"
@@ -43,14 +40,12 @@ USER_PASSWORD = "test"
 ENABLE_PASSWORD = ""
 
 # firewall definitions here
-firewalls = ['1.1.1.1',     # FW1
-			 '2.2.2.2',     # FW2
-			 '3.3.3.3',]     # FW3
+firewalls = ['10.1.1.1']     # FW1
 
 # our function for pexpect and logging into a server to shun
 def shun(firewall, attacker, action, USERNAME, USER_PASSWORD, ENABLE_PASSWORD):
 	# spawn a child thread to execute our SSH command
-	child = pexpect.spawn("ssh %s@%s" % (USERNAME, firewall))
+	child = pexpect.spawn("ssh %s@%s"%(USERNAME, firewall))
 	# wait for the password
 	child.expect("assword:", timeout=5)
 	# send the password
@@ -65,28 +60,14 @@ def shun(firewall, attacker, action, USERNAME, USER_PASSWORD, ENABLE_PASSWORD):
 	# if we are using a different enable password
 	if ENABLE_PASSWORD != "":
 		child.sendline(ENABLE_PASSWORD)
-	# wait for enable prompt
-	child.expect("#", timeout=5)
-	# configure the terminal
-	child.sendline("configure terminal")
-	# wait for enable prompt
-	child.expect("#", timeout=5)
-	# defined group for network blocked sites
-	child.sendline("object-group network blocked_sites")
-	# wait for enable prompt
-	child.expect("#", timeout=5)
-    
 	# if we are looking to shun the attacker IP address
-	if action == 1:
+	if action == "1":
 		command = "shun " + attacker
-    
 	# if we are looking to unshun
-	if action == 2:
+	if action == "2":
 		command = "no shun " + attacker
-    
 	# shun or unshun the attacker
 	child.sendline(command)
-    
 	# wait for the expected response
 	child.expect("#", timeout=5)
 	# exit out of the system
@@ -102,23 +83,44 @@ try:
 	# 1 equals a shun, 2 equals unshun
 	action = sys.argv[2]
 
+	
 # if we were not passed the appropriate command line arguments then trigger an alert
 except IndexError:
 	# print that the proper commands were not given
 	print "\n####################################################################"
 	print "#"
-	print "# Automatic Shunning Script for ArcSight"
+	print "# Automatic Shunning Script"
 	print "#"
 	print "# Takes in two parameters, attacker IP address and shun or unshun"
 	print "#"
 	print "# Note: python-pexpect will need to be installed on the machine"
 	print "#"
 	print "# Written by: TrustedSec - David Kennedy"
+	print "# Basterized By Justin Herman"
 	print "#"
 	print "####################################################################"
 	print "\n[!] The correct command arguments were not given to the shun tool."
 	print "[!] Options: Two parameters must be passed, IP Address, shun/1 or unshun/2," 
-        print "             and AES cipher key."
-	print "\nUsage: python shun.py <ip-address> <1 or 2> <AES encryption key> "
+        print "\n"
+	print "\nUsage: python shun.py <ip-address> <1 or 2> "
 	sys.exit()
 
+
+# wrap everything in a try/except block for error catching
+try:
+	for firewall in firewalls:
+		attacker = sys.argv[1]
+		action = sys.argv[2]
+	# start the shunning process
+		shun(firewall, attacker, action, USERNAME, USER_PASSWORD, ENABLE_PASSWORD)
+	# if we are shunning
+	if action == "1":
+		print "Successfully shunned IP Address: %s on FW %s" %(attacker,firewall)
+	# if we are unshunning
+	if action == '2':
+		print "Successfully unshunned IP Address: %s on FW %s" % (attacker,firewall)
+
+# catch all errors and write it out to an error log
+except:
+	print "Error has occured"
+	sys.exit()
